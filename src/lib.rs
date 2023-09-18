f7053416d44f10595e4312f446a65a3048e63336
Josiah Denton <jfranklindenton@gmail.com>
2023-09-18 15:30:08 -0400

    showing off htmx working

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,10 @@ use flexi_logger::Logger;
 use tower_http::services::ServeFile;
 
 pub fn setup_routes() -> Router {
+    // seperates routes at the feature level
+    // each feature will be a new route
     Router::new().route("/", routing::get(routes::index))
+        .route("/click", routing::post(routes::click))
         .nest_service("/styles.css", ServeFile::new("templates/styles/styles.css"))
         .nest_service("/htmx.min.js", ServeFile::new("templates/scripts/htmx.min.js"))
 }
--- src/routes.rs
+++ src/routes.rs
@@ -5,3 +5,7 @@ const NAME: &'static str = "Josiah";
 pub async fn index() -> IndexTemplate<'static> {
    IndexTemplate::new(NAME)
 }
+
+pub async fn click() -> String {
+    String::from("<p>You clicked me!</p>")
+}
